4689342ef357655005ab3b0145820e4abc419f22
kausek <jakub.kausa@gmail.com>
2024-09-19 15:12:53 +0200

    creating angular project

---
+++ ecommerce-frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    ],
+  providers: [
+    ProductService, 
+    provideHttpClient(withInterceptorsFromDi()) 
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
